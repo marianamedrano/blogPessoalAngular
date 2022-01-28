941af96836b70cc21f3913a85a20b288e6587bfc
Mariana de Cássia Medrano <maremedrano@yahoo.com>
2022-01-28 18:44:31 -0300

    Início do crud de postagens

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UserLogin } from '../model/UserLogin';
@@ -14,12 +14,26 @@ export class AuthService {
     private http: HttpClient
   ) { }
 
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token),
+};
+
+  refreshToken() {
+  this.token = {
+    headers: new HttpHeaders().set('Authorization', environment.token),
+  };
+}
+
   entrar(userLogin: UserLogin): Observable<UserLogin>{
     return this.http.post<UserLogin>('https://pelasruasqueandei.herokuapp.com/usuarios/logar', userLogin)
   }
 
   cadastrar(user: User): Observable<User>{
     return this.http.post<User>('https://pelasruasqueandei.herokuapp.com/usuarios/cadastrar', user)
+  }
+
+  getByIdUser(id: number): Observable<User>{
+    return this.http.get<User>(`https://pelasruasqueandei.herokuapp.com/usuarios/${id}`, this.token)
   }
 
   logado(){
@@ -29,5 +43,7 @@ export class AuthService {
     }
     return ok  
   }
+
+  
   
 } 
