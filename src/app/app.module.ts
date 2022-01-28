3ecc2f51e3492dc641220c80ccfe2081ecc99726
Mariana de Cássia Medrano <maremedrano@yahoo.com>
2022-01-27 21:12:08 -0300

    CRUD de tema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { MenulogadoComponent } from './menulogado/menulogado.component';
 import { TemaComponent } from './tema/tema.component';
+import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
+import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,9 @@ import { TemaComponent } from './tema/tema.component';
     CadastrarComponent,
     InicioComponent,
     MenulogadoComponent,
-    TemaComponent
+    TemaComponent,
+    TemaEditComponent,
+    TemaDeleteComponent
   ],
   imports: [
     BrowserModule,
--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -9,17 +11,38 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class TemaComponent implements OnInit {
 
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+
   constructor(
-    private router: Router
+    private router: Router,
+    private temaService: TemaService
   ) { }
 
   ngOnInit() {
     window.scroll(0,0)
 
+    this.findAllTemas()
+
     if(environment.token == ''){
       alert('Sua sessão expirou. Faça o login novamente.')
       this.router.navigate(['/entrar'])
     }
+  }
+
+  cadastrar(){
+    this.temaService.postTema(this.tema).subscribe((resp: Tema)=>{
+    this.tema = resp
+    alert('Tema cadastrado com sucesso!')
+    this.tema = new Tema()
+    this.findAllTemas()
+    })
+  }
+  
+  findAllTemas(){
+    this.temaService.getAllTema().subscribe((resp:Tema[]) => {
+    this.listaTemas = resp
+    })
   }
 
 }
